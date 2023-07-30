561246c21f6c633b8d719e4631ccd53711bd9db6
gibet <gibet@10.0.0.234>
2023-07-29 23:35:44 -0400

    new method

--- src/Day3/DemoDay3.java
+++ src/Day3/DemoDay3.java
@@ -4,18 +4,9 @@ public class DemoDay3 {
 public static void main(String[] args) {
 	
 	System.out.println("Gibet is crazy");
-<<<<<<< HEAD
-}
-public void myName(){
 
-=======
 	System.out.println("Gibet Telma");
 	
 }
-	public void myName(String name, String Lastname){
->>>>>>> branch 'master' of https://github.com/gibettelma1/GitHubProject2.git
-	String name= "Gibet";
-	String lastName= "Telma";
-	System.out.println(name + '"' + lastName);
-}
+	
 }
