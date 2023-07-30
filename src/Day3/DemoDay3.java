740cdb8cff5610b6e3039f0cd67631fe790b3f18
gibettelma1 <140761564+gibettelma1@users.noreply.github.com>
2023-07-29 23:32:58 -0400

    Update DemoDay3.java

--- src/Day3/DemoDay3.java
+++ src/Day3/DemoDay3.java
@@ -2,7 +2,10 @@ package Day3;
 
 public class DemoDay3 {
 public static void main(String[] args) {
+	
 	System.out.println("Gibet is crazy");
+	System.out.println("Gibet Telma");
+	
 }
 	public void myName(String name, String Lastname){
 	String name= "Gibet";
