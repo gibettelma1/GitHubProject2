8452ab879b9153711a8f491968d5cb16b457a0b5
gibet <gibet@10.0.0.234>
2023-07-29 23:33:58 -0400

    new method

--- src/Day3/DemoDay3.java
+++ src/Day3/DemoDay3.java
@@ -3,9 +3,11 @@ package Day3;
 public class DemoDay3 {
 public static void main(String[] args) {
 	System.out.println("Gibet is crazy");
-public void myName(String name, String Lastname){
+}
+public void myName(){
+
 	String name= "Gibet";
 	String lastName= "Telma";
-	System.out.println(name + " + lastName);
+	System.out.println(name + '"' + lastName);
 }
 }
